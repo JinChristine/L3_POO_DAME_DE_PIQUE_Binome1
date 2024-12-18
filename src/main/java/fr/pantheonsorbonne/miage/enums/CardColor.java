e35a172b84a4a613dc5cbac9509554744f4fb1da
Steven200405 <huangsteven2004@gmail.com>
2024-12-18 23:06:06 +0000

    Temp

--- src/main/java/fr/pantheonsorbonne/miage/enums/CardColor.java
+++ src/main/java/fr/pantheonsorbonne/miage/enums/CardColor.java
@@ -1,7 +1,21 @@
 package fr.pantheonsorbonne.miage.enums;
 
 public enum CardColor {
-    HEART, SPADE, CLUB, DIAMOND;
+    SPADE(127137),
+    HEART(127137 + 16),
+    DIAMOND(127137 + 16 * 2),
+    CLUB(127137 + 16 * 3);
+
+    private final int code;
+    CardColor(int code){
+        this.code = code;
+    }
+
+    
+
+    public int getCode(){
+        return code;
+    }
 
     public String getStringRepresentation(CardColor color) {
         return color.toString();
