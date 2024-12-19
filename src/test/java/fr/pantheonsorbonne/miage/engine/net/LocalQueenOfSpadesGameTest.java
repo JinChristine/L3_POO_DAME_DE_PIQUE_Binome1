b3c8ba033ae7207b4d8ca590c33ed6a86d6fb77e
JinChristine <jin.christine_0111@hotmail.com>
2024-12-19 11:17:59 +0100

    update LocalQueenOfSpadesGameTest

--- src/test/java/fr/pantheonsorbonne/miage/engine/net/LocalQueenOfSpadesGameTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/engine/net/LocalQueenOfSpadesGameTest.java
@@ -1,5 +1,49 @@
 package fr.pantheonsorbonne.miage.engine.net;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import fr.pantheonsorbonne.miage.game.Card;
+import fr.pantheonsorbonne.miage.game.DumpPlayer;
+import fr.pantheonsorbonne.miage.game.Player;
+import fr.pantheonsorbonne.miage.game.SmartPlayer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import fr.pantheonsorbonne.miage.engine.local.LocalQueenOfSpadesGame;
+import fr.pantheonsorbonne.miage.enums.CardColor;
+import fr.pantheonsorbonne.miage.enums.CardValue;
+
+
 public class LocalQueenOfSpadesGameTest {
+    private Queue<Player> players;
+    private LocalQueenOfSpadesGame game;
+
+    @BeforeEach
+    void setUp() {
+        players = new LinkedList<>();
+        players.add(new SmartPlayer("player1"));
+        players.add(new SmartPlayer("player2"));
+        players.add(new DumpPlayer("player3"));
+        players.add(new DumpPlayer("player4"));
+        game = new LocalQueenOfSpadesGame(players);
+    }
+    
+    @Test
+    void testGetWinnerCard() {
+        Queue<Card> roundDeck = new LinkedList<>();
+        roundDeck.add(new Card(CardValue.THREE, CardColor.HEART)); 
+        roundDeck.add(new Card(CardValue.QUEEN, CardColor.HEART)); 
+        roundDeck.add(new Card(CardValue.NINE, CardColor.HEART)); 
+        roundDeck.add(new Card(CardValue.ACE, CardColor.SPADE));
+
+        Card winnerCard = game.getWinnerCard(roundDeck);
+
+        assertEquals(12, winnerCard.getValue().getRank());
+        assertEquals(CardColor.HEART, winnerCard.getColor());
+    }
     
 }
